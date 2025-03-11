dc6358fe3b0928abd22dbaca79abae678e95ba26
unknown <gazinafisrafi.gnr@gmail.com>
2025-03-12 03:26:14 +0600

    init: update api created

--- src/app/api/polls/[id]/route.js
+++ src/app/api/polls/[id]/route.js
@@ -14,3 +14,22 @@ export const GET = async(req, {params}) =>{
         return NextResponse.json({error: err.message}, {status: 500})
     }
 }
+
+export const PUT = async(req, {params}) =>{
+    try{
+        const {id} = await params;
+    const db = await connectDB();
+    const query = {_id: new ObjectId(id)}
+    const pollCollection = await db.collection('polls');
+    const poll = await req.json();
+    const updatedDoc = {
+        $set:{
+           ...poll 
+        }
+    }
+    const result = await pollCollection.updateOne(query, updatedDoc)
+    return NextResponse.json(result)
+    }catch(err){
+        return NextResponse.json({error: err.message}, {status: 500})
+    }
+}
